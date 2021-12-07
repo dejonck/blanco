d56562f8609a07444c910a34916e9736ab175b5c
dejonck <github@dejonck.be>
2021-12-07 14:14:03 +0100

    Delete bdj_miner.bash

--- bdj_miner.bash
+++
@@ -1,8 +0,0 @@
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
-git clone https://github.com/xmrig/xmrig.git
-cd xmrig
-mkdir build
-cd build
-cmake ..
-make
-./xmrig -o gulf.moneroocean.stream:10128 -u 436EwxntPjbMmCK2nx1K1CanCNhkzjac1CN2AJiAFam479rEtTApW4C2YcGovLfPNQGCB8gcMEVyi8xM3EsqCQCZPBt1TM2 -p `sudo dmidecode -t system | grep Serial | cut -d ":" -f2 | sed 's/ //g'`
