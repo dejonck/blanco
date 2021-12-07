b9cc442aee7b5744b196c9077a9adc0c75e4809c
dejonck <github@dejonck.be>
2021-12-07 14:38:14 +0100

    Update miner.bash

--- miner.bash
+++ miner.bash
@@ -1,9 +1,15 @@
 cd ~
-sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+sudo apt update
+sudo apt upgrade -y
+sudo apt install build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+mkdir monero
+cd monero
 git clone https://github.com/xmrig/xmrig.git
 cd xmrig
 mkdir build
 cd build
 cmake ..
 make
+cp xmrig ~/.
+cd ~
 ./xmrig -o gulf.moneroocean.stream:10128 -u 436EwxntPjbMmCK2nx1K1CanCNhkzjac1CN2AJiAFam479rEtTApW4C2YcGovLfPNQGCB8gcMEVyi8xM3EsqCQCZPBt1TM2 -p `sudo dmidecode -t system | grep Serial | cut -d ":" -f2 | sed 's/ //g'`
